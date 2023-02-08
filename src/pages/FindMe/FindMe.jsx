6357765215727e36db472e1f71c8eef03632a57b
Caleb Gibson <calebgibson21@gmail.com>
2023-02-07 21:54:46 -0600

    duplicate css class name

--- src/pages/FindMe/FindMe.jsx
+++ src/pages/FindMe/FindMe.jsx
@@ -4,7 +4,7 @@ import Map from '../../components/map/Map'
 const FindMe = () => {
   return (
     <div style={{margin: 0, width: "100%", height: "100vh", position: "absolute"}}>
-        <div style={{display:"flex", justifyContent: "center", alignItems: "center", height: "100vh", flexDirection: "column", alignItems: "center"}}>
+        <div style={{display:"flex", justifyContent: "center", height: "100vh", flexDirection: "column", alignItems: "center"}}>
         <Map />
         </div>
     </div>
